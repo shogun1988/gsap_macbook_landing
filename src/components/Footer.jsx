db30f3dcdfc87e7623660061a2630275a4001a9a
shogun1988 <nokahvandenhoven@gmail.com>
2025-11-21 20:28:49 +0100

    implement current year function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,23 @@
 import {footerLinks} from "../constants/index.js";
 
+
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
         <footer>
             <div className="info">
                 <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
-                <img src="/logo.svg" alt ="Apple logo"/>
+                <img src="/logo.svg" alt="Apple logo" />
             </div>
 
             <hr />
 
             <div className="links">
-                <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+                <p>Copyright © {year} Apple Inc. All rights reserved.</p>
 
                 <ul>
-                    {footerLinks.map(({label, link }) => (
+                    {footerLinks.map(({ label, link }) => (
                         <li key={label}>
                             <a href={link}>{label}</a>
                         </li>
